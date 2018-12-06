0d02b11f99ac482705e6c7029b4153b6b5b8a415
Samuel <sumlebulle@hotmail.com>
2018-12-06 13:39:28 +0100

    Pasted alot of slick things

---
+++ slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.slider-desktop').slick({
+      prevArrow: '<img class="slide-left" src="images/slider_arrow_mobile.svg">',
+      nextArrow: '<img class="slide-right" src="images/slider_arrow_mobile.svg">',
+      mobileFirst: true
+  });
+});
